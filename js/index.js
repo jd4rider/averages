1de1335ad7f1e38549a83835d9cf1e3c359dc568
jd4rider <jd4rider@gmail.com>
2017-10-29 12:29:54 -0600

    try

--- js/index.js
+++ js/index.js
@@ -58,3 +58,11 @@ function average(){
   return avg;
     
 }
+
+//navigation fix on mobile
+
+$(document).on('blur', 'input, textarea', function () {
+    setTimeout(function () {
+        window.scrollTo(document.body.scrollLeft, document.body.scrollTop);
+    }, 0);
+});
