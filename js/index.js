c962d1cf091a68cdf45b4e6d74add42920abd948
Nicole Forrider <na4rider@Nicoles-MacBook-Air.local>
2017-10-29 23:05:44 -0600

    woot

--- js/index.js
+++ js/index.js
@@ -12,52 +12,50 @@ $('#avgform').on('input', function(){
 //helper functions
 
 function getmax(){
-  
+
   var formarr = [];
   var formarrnum = [];
   var maxinput = 2;
   var maxnuminput;
   var nextmax = maxinput + 1;
-  
+
   $('.form-control').each(function(i){
     formarr.push(parseInt($(this).attr('id')));
   })
-  
+
   $('.form-control').each(function(i){
-    
+
     if(!isNaN(parseInt($(this).val()))){
         formarrnum.push(parseInt($(this).attr('id')));
      }
-    
+
   })
-  
+
   maxinput = Math.max.apply(null, formarr);
   maxnuminput = formarrnum.length;
   nextmax = maxinput + 1;
-  
+
   return [maxinput,nextmax, maxnuminput];
 }
 
 function addmore(){
-    $('#inputgrp').append('<input class="form-control" id='+getmax()[1]+' style="text-align:center;"></input>');
+    $('#inputgrp').append('<input type="number" class="form-control" id='+getmax()[1]+' style="text-align:center;"></input>');
 }
 
 function average(){
   var total =0;
   var avg;
-  
+
   $('.form-control').each(function(i){
       if(!isNaN(parseInt($(this).val()))){
         total = total + parseInt($(this).val())
       }
-    
+
    })
-   
+
   avg = total / (getmax()[2]);
-   
-       
+
+
   return avg;
-    
+
 }
-
-
